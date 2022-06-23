5b67fcafc11e8256b4afe84ed48bf254e86994ba
Thomas <malifarge.thomas9@gmail.com>
2022-06-23 14:35:48 +0200

    Combine

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ console.log(cat.age);
 if (cat.isCute){
     console.log("So cute !");
 }
+
+// 02 - Combine
+
+const cat2 ={
+
+    name: "Isidors",
+    age : 5,
+    isCute :false
+    }
+
+    let cats = [ cat,cat2]
+
+  console.log(cats[0].age);
+  console.log(cats[1].isCute);  
